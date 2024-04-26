c0f676635491b04787feeb7cd8a5e1b1b618ceae
pallavithorat <thoratpallavi2000@gmail.com>
2024-04-25 21:28:39 -0700

    Changes to API URL

--- clients/api.ts
+++ clients/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 
 const isClient = typeof window !== 'undefined';
 //https://d25pn6flyt9tb9.cloudfront.net/graphql
-export const graphqlClient = new GraphQLClient("https://d25pn6flyt9tb9.cloudfront.net/graphql",{
+export const graphqlClient = new GraphQLClient("process.env.NEXT_PUBLIC_API_URL as string",{
     headers: () => ({
         Authorization: isClient ? `Bearer ${window.localStorage.getItem('__twitter_token')}`:"",
     }),
